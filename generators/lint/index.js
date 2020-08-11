9df8734c021fafb82aaad78dc68c4c035c3770eb
Dennis Lien <dennis.lien.o@gmail.com>
2020-08-11 14:15:18 +0200

    Update sub lint

--- generators/lint/index.js
+++ generators/lint/index.js
@@ -1,40 +1,41 @@
 const Generator = require('yeoman-generator');
 
+const { template } = require('../../lib/template');
+
 /**
  * Add Lint to a project
  */
 module.exports = class extends Generator {
-  writing() {
+  constructor(args, opts) {
+    super(args, opts);
+    this.deps = {
+      dev: [],
+      prod: [],
+    };
+    this.pkgScripts = {};
+    this.ctx = {};
+  }
+
+  addLint() {
+    const [lintProdDeps, lintDevDeps, lintScripts] = template
+      .createLint(this, './');
+
+    this.deps.prod.push(...lintProdDeps);
+    this.deps.dev.push(...lintDevDeps);
+    Object.assign(this.pkgScripts, lintScripts);
+  }
+
+  install() {
+    const scripts = this.pkgScripts;
     const pkgJson = {
       scripts: {
-        lint: 'eslint --ignore-path .eslintignore ./',
-      },
-      devDependencies: {
-        'eslint': '^7.6.x',
-        'eslint-config-airbnb-base': '^14.2.x',
-        'eslint-plugin-chai-friendly': '^0.6.x',
-        'eslint-plugin-import': '^2.22.x',
+        ...scripts,
       },
     };
 
     // Extend or create package.json file in destination path
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
-
-    this.fs.copyTpl(
-      this.templatePath('.eslintignore'),
-      this.destinationPath('.eslintignore'),
-    );
-    this.fs.copyTpl(
-      this.templatePath('.eslintrc.js'),
-      this.destinationPath('.eslintrc.js'),
-    );
-  }
-
-  install() {
-    this.npmInstall();
-    // this.npmInstall(['eslint'], { 'save-dev': true });
-    // this.npmInstall(['eslint-config-airbnb-base'], { 'save-dev': true });
-    // this.npmInstall(['eslint-plugin-chai-friendly'], { 'save-dev': true });
-    // this.npmInstall(['eslint-plugin-import'], { 'save-dev': true });
+    this.npmInstall(this.deps.prod, { 'save-dev': false });
+    this.npmInstall(this.deps.dev, { 'save-dev': true });
   }
 };
