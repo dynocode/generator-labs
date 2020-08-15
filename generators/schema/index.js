00bd83e0afb9a0b57e2518a5efbc3e7880cf1d93
Dennis Lien <dennis.lien.o@gmail.com>
2020-08-15 14:22:33 +0200

    Basic schema gen by model def

--- generators/schema/index.js
+++ generators/schema/index.js
@@ -4,13 +4,15 @@ const Generator = require('../../lib/generator/base');
 
 const { template } = require('../../lib/template');
 const { getFilePathToAllFilesInDir } = require('../../lib/fs');
+const { getAstFromCode, getModelDefFromAst } = require('../../lib/AST/mongo-model');
+const { createSchemaFromModelDef } = require('../../lib/schema');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.argument('schemaName', { type: String, desc: 'name of the file and the schema', required: false });
-    this.required = ['modelsDir', 'schemaDir'];
-    this.modelBase = '';
+    this.required = ['modelsDir', 'schemaDir', 'importExport'];
+    this.modelBasePath = '';
   }
 
   async init() {
@@ -53,69 +55,94 @@ module.exports = class extends Generator {
       }
 
       this.modelAsk = await this.prompt([ask]);
-      this.modelBase = path.join(this.ctx.modelsDir, this.modelAsk.baseModel);
+      this.modelBasePath = path.join(this.ctx.modelsDir, this.modelAsk.baseModel);
     }
   }
 
-  setUpSchema() {
-    const { ctx } = this;
-    if (!ctx.schemaDir) {
-      this.log.info('Schema not set up, setting up now... /n');
-      const [schemaProdDeps, schemaDevDeps, schemaScripts] = template
-        .createSchema(this, ctx.srcPath || './src', {
-          importExport: ctx.importExport || true,
-        });
-
-      this.deps.prod.push(...schemaProdDeps);
-      this.deps.dev.push(...schemaDevDeps);
-      Object.assign(this.pkgScripts, schemaScripts);
+  getBaseModel() {
+    if (!this.useModelAsBase || !this.modelBasePath) {
+      return null;
+    }
+    const modelPath = this.modelBasePath;
+    try {
+      const file = this.fs.read(modelPath);
+      const ast = getAstFromCode(file);
+      const modelDef = getModelDefFromAst(ast);
+      const schemaData = createSchemaFromModelDef(modelDef);
+      const query = Object.values(schemaData.query).join('\n');
+      const mutations = Object.values(schemaData.mutations).join('\n');
+      template.createNewSchemaWithDef(this, this.ctx.schemaDir, this.options.schemaName, {
+        ...this.ctx,
+        query,
+        mutations,
+        typeDef: schemaData.typeDef,
+      });
+    } catch (error) {
+      console.log('********  **********');
+      console.log(error);
+      console.log('********  END *********');
     }
   }
 
-  newSchema() {
-    const { ctx } = this;
-    if (ctx.schemaDir) {
-      this.log('Schema already set up, creating new schema... \n');
-      if (!this.options.schemaName) {
-        this.log.error('Missing schema name: yo labs:schema [name] \n');
-        this.log(this.help());
-        process.exit(1);
-      }
-      const fileName = this.options.schemaName
-        .trim()
-        .toLowerCase();
+  // setUpSchema() {
+  //   const { ctx } = this;
+  //   if (!ctx.schemaDir) {
+  //     this.log.info('Schema not set up, setting up now... /n');
+  //     const [schemaProdDeps, schemaDevDeps, schemaScripts] = template
+  //       .createSchema(this, ctx.srcPath || './src', {
+  //         importExport: ctx.importExport || true,
+  //       });
 
-      const schemaName = fileName.split('-').map((item) => {
-        const firstChar = item.substring(0, 1).toUpperCase();
-        return `${firstChar}${item.substring(1)}`;
-      }).join('');
+  //     this.deps.prod.push(...schemaProdDeps);
+  //     this.deps.dev.push(...schemaDevDeps);
+  //     Object.assign(this.pkgScripts, schemaScripts);
+  //   }
+  // }
 
-      const [schemaProdDeps, schemaDevDeps, schemaScripts] = template
-        .createNewSchema(this, ctx.srcPath || './src', fileName, {
-          importExport: ctx.importExport || true,
-          name: schemaName,
-        });
+  // newSchema() {
+  //   const { ctx } = this;
+  //   if (ctx.schemaDir) {
+  //     this.log('Schema already set up, creating new schema... \n');
+  //     if (!this.options.schemaName) {
+  //       this.log.error('Missing schema name: yo labs:schema [name] \n');
+  //       this.log(this.help());
+  //       process.exit(1);
+  //     }
+  //     const fileName = this.options.schemaName
+  //       .trim()
+  //       .toLowerCase();
 
-      this.deps.prod.push(...schemaProdDeps);
-      this.deps.dev.push(...schemaDevDeps);
-      Object.assign(this.pkgScripts, schemaScripts);
-    }
-  }
+  //     const schemaName = fileName.split('-').map((item) => {
+  //       const firstChar = item.substring(0, 1).toUpperCase();
+  //       return `${firstChar}${item.substring(1)}`;
+  //     }).join('');
 
-  install() {
-    const { ctx } = this;
-    if (!ctx.schemaDir) {
-      const scripts = this.pkgScripts;
-      const pkgJson = {
-        scripts: {
-          ...scripts,
-        },
-      };
+  //     const [schemaProdDeps, schemaDevDeps, schemaScripts] = template
+  //       .createNewSchema(this, ctx.srcPath || './src', fileName, {
+  //         importExport: ctx.importExport || true,
+  //         name: schemaName,
+  //       });
 
-      // Extend or create package.json file in destination path
-      this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
-      this.npmInstall(this.deps.prod, { 'save-dev': false });
-      this.npmInstall(this.deps.dev, { 'save-dev': true });
-    }
-  }
+  //     this.deps.prod.push(...schemaProdDeps);
+  //     this.deps.dev.push(...schemaDevDeps);
+  //     Object.assign(this.pkgScripts, schemaScripts);
+  //   }
+  // }
+
+  // install() {
+  //   const { ctx } = this;
+  //   if (!ctx.schemaDir) {
+  //     const scripts = this.pkgScripts;
+  //     const pkgJson = {
+  //       scripts: {
+  //         ...scripts,
+  //       },
+  //     };
+
+  //     // Extend or create package.json file in destination path
+  //     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+  //     this.npmInstall(this.deps.prod, { 'save-dev': false });
+  //     this.npmInstall(this.deps.dev, { 'save-dev': true });
+  //   }
+  // }
 };
